cd4327e46eb4f60dc0d465a1c45270c8e581666a
prrt714 <prrt714@gmail.com>
2015-03-12 01:51:28 +0300

    v0.0.2

--- data/modify.js
+++ data/modify.js
@@ -1,34 +1,36 @@
 const regex = /^(?:https?:\/\/)?(?:www\.)?(?:vesti.ru)?\/doc.html\?id=(\d+).*$/i;
 
-var scanAndModify = function (elm) {
-  var article = elm.getElementsByClassName("article"), f;
-  if (!article) return;
-  for (var i in article) {
-    f = article[i];
-    if (f.tagName !== "DIV") continue;
-    var videoLink = f.querySelector(".article__video-link");
-    if (!videoLink) continue;
-    var docId;
-    var share = f.querySelector("span[data-yasharelink]");//getElementsByClassName("share")
-    if (!share) continue;
-    docId = share.getAttribute("data-yasharelink").match(regex);
-    if (!docId) continue;
-    docId = docId[1];
-    videoLink.href = "http://mobile.vesti.ru/v/" + docId + ".480.mp4";
-    videoLink.removeAttribute("data-video-url");
-    videoLink.setAttribute("target", "_blank");
-    videoLink.classList.remove("article__video-link");
-    videoLink.style.textDecoration = "none";
-    videoLink.style.position = "relative";
-    videoLink.style.display = "block";
-  }
+let debounce = function (fn, timeout, invokeAsap) {
+  var timer;
+  return function () {
+    var args = arguments;
+    invokeAsap && !timer && fn.apply(this, args);
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      !invokeAsap && fn.apply(this, args);
+      timer = null;
+    }, timeout);
+  };
 };
 
-var observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    scanAndModify(mutation.target);
-  });
-});
+let scanAndModify = debounce(function () {
+  var article = document.querySelector(".article");
+  if (!article) return;
+  if (article.tagName !== "DIV") return;
+  var videoLink = article.querySelector("a[data-video-url]");
+  if (!videoLink) return;
+  var docId;
+  var share = article.querySelector("span[data-yasharelink]");//getElementsByClassName("share")
+  if (!share) return;
+  docId = share.getAttribute("data-yasharelink").match(regex);
+  if (!docId) return;
+  docId = docId[1];
+  videoLink.href = "http://mobile.vesti.ru/v/" + docId + "." + self.options.quality + ".mp4";
+  videoLink.removeAttribute("data-video-url");
+  videoLink.removeAttribute("data-video-type");
+  videoLink.setAttribute("target", "_blank");
+}, 100, true);
 
-// pass in the target node, as well as the observer options
+let observer = new MutationObserver(scanAndModify);
+
 observer.observe(document, { childList: true, subtree: true });
